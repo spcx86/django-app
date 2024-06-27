d26a192734fe3c14d0dc38f9bc982b490ea2b811
spcx86 <123968951+spcx86@users.noreply.github.com>
2024-06-27 20:23:54 +0530

    update urls

--- django_app/urls.py
+++ django_app/urls.py
@@ -1,12 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 from myapp.views import HelloWorldView, callback
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
-    path('accounts/', include('allauth.urls')),  # Move this up
+    path('accounts/', include('allauth.urls')),
     path('', include('django.contrib.auth.urls')),
     path('api/', include('myapp.urls')),
     path('callback/', callback, name='callback'),
-]
+    path('favicon.ico', RedirectView.as_view(url='/static/favicon.ico', permanent=True)),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
